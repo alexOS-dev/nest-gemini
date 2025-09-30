69e53a6df105de416c9a4f9717b799fe01116579
Alex Oliva <alexoliva.developer@gmail.com>
2025-09-30 02:43:35 -0300

    feat: Add basicPromptStream endpoint to stream responses from GeminiService

--- src/gemini/gemini.controller.ts
+++ src/gemini/gemini.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { GeminiService } from './gemini.service';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { type Response } from 'express';
 
 @Controller('gemini')
 export class GeminiController {
@@ -16,5 +17,24 @@ export class GeminiController {
   @Post('basic-prompt')
   basicPrompt(@Body() basicPromptDto: BasicPromptDto) {
     return this.geminiService.basicPrompt(basicPromptDto);
+  }
+  @Post('basic-prompt-stream')
+  async basicPromptStream(
+    @Body() basicPromptDto: BasicPromptDto,
+    @Res() res: Response,
+    // TODO: Files
+  ) {
+    const stream = await this.geminiService.basicPromptStream(basicPromptDto);
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(HttpStatus.OK);
+
+    for await (const chunk of stream) {
+      const piece = chunk.text;
+      console.log(piece);
+      res.write(piece);
+    }
+
+    res.end();
   }
 }
