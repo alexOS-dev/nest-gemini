9ca1f32e82d5b150da9f11d7636e8b4f208be0e3
Alex Oliva <alexoliva.developer@gmail.com>
2025-09-30 02:43:39 -0300

    feat: Implement basicPromptStream method in GeminiService to handle streaming responses

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -2,6 +2,7 @@ import { GoogleGenAI } from '@google/genai';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
 import { Injectable } from '@nestjs/common';
 import { basicPromptUseCase } from './use-cases/basic-prompt.use-case';
+import { basicPromptStreamUseCase } from './use-cases/basic-prompt-stream.use-case';
 
 @Injectable()
 export class GeminiService {
@@ -9,5 +10,8 @@ export class GeminiService {
 
   async basicPrompt(basicPromptDto: BasicPromptDto) {
     return basicPromptUseCase(this.ai, basicPromptDto);
+  }
+  async basicPromptStream(basicPromptDto: BasicPromptDto) {
+    return basicPromptStreamUseCase(this.ai, basicPromptDto);
   }
 }
